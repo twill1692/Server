7c8e18487b658d24086c3f024b17f6061a8e613d
twill1692 <twill1692@gmail.com>
2020-12-15 18:58:40 -0600

    name changes

--- src/main/java/com/app/player/Player.java
+++ src/main/java/com/app/player/Player.java
@@ -1,5 +1,14 @@
 package com.app.player;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class Player {
-    
+
+@RequestMapping("/playerCreate")
+public String createPlayer(){
+    return "Created";  
+}
+
 }
---
+++ src/main/java/com/app/player/PlayerDAO.java
@@ -0,0 +1,6 @@
+//The interface which will be used by PlayerEntity class and the PlayerController class. 
+package com.app.player;
+
+public interface PlayerDAO {
+    
+}
