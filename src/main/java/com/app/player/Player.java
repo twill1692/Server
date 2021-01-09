885591b8798b054dbb02af76798721d75a39cd48
twill1692 <twill1692@gmail.com>
2021-01-09 16:25:45 -0600

    ...

--- src/main/java/com/app/player/Player.java
+++ src/main/java/com/app/player/Player.java
@@ -1,14 +1,43 @@
 package com.app.player;
 
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class Player {
+public class Player implements PlayerDAO {
 
-@RequestMapping("/playerCreate")
-public String createPlayer(){
-    return "Created";  
-}
+    @Override
+    public List<Player> getAllPlayers() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Player getPlayer(int playerID) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public void updatePlayer(Player player) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void deleteplayert(Player player) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @RequestMapping("/playerCreate")
+    @Override
+    public String createPlayer(Player player) {
+        return "Hi, you created a player";
+    }
+
+
 
 }
--- src/main/java/com/app/player/PlayerDAO.java
+++ src/main/java/com/app/player/PlayerDAO.java
@@ -1,6 +1,20 @@
 //The interface which will be used by PlayerEntity class and the PlayerController class. 
 package com.app.player;
 
+import java.util.List;
+
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 public interface PlayerDAO {
+    public List<Player> getAllPlayers();
+
+    public Player getPlayer(int playerID);
+
+    @RequestMapping("/playerCreate")
+    public String createPlayer(Player player);
     
+    public void updatePlayer(Player player);
+
+    public void deleteplayert(Player player);
 }
